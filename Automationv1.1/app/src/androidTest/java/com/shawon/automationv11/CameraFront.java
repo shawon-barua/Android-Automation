a1c11843141cccff90bb956ae966b2b63c152ac2
shawon-barua <shawon.cse.ku@gmail.com>
2016-01-06 17:00:23 +0600

    3 file added
    direction , front and rear added

--- Automationv1.1/app/src/androidTest/java/com/shawon/automationv11/CameraFront.java
+++ Automationv1.1/app/src/androidTest/java/com/shawon/automationv11/CameraFront.java
@@ -3,10 +3,11 @@ package com.shawon.automationv11;
 import android.support.test.uiautomator.By;
 import android.support.test.uiautomator.UiDevice;
 import android.support.test.uiautomator.UiObject2;
+import android.support.test.uiautomator.UiSelector;
 import android.support.test.uiautomator.Until;
 import android.test.InstrumentationTestCase;
 
-public class CameraRear extends InstrumentationTestCase {
+public class CameraFront extends InstrumentationTestCase{
 
     private UiDevice device;
 
@@ -14,8 +15,8 @@ public class CameraRear extends InstrumentationTestCase {
     public void setUp() throws Exception {
 
         device = UiDevice.getInstance(getInstrumentation());
-        device.pressHome();
-        device.waitForIdle(5000);
+        //device.pressHome();
+
         // Wait till the Apps icon is on the screen
         //device.wait(Until.hasObject(By.desc("Apps")), 3000);
 
@@ -27,32 +28,29 @@ public class CameraRear extends InstrumentationTestCase {
         device.wait(Until.hasObject(By.text("Camera")), 3000);
         UiObject2 cameraApp = device.findObject(By.text("Camera"));
         cameraApp.click();
-        device.waitForIdle(5000);
+        //device.waitForIdle(5000);
        // UiObject2 calculatorApp = device.findObject(By.text("Calculator"));
        // calculatorApp.click();
 
     }
 
-    public void testRear() throws Exception {
+    public void testAdd() throws Exception {
 
         device.wait(Until.hasObject(By.res("com.android.camera:id/camera_bottom_bar")), 1000);
 
-       // UiObject2 capture1 = device.findObject(By.res("com.android.camera:id/shutter_indicator_view"));
-       // UiObject2 capture1 = device.findObject(By.res("com.android.gallery3d:id/shutter_button_photo"));
-        UiObject2 capture1 = device.findObject(By.res("com.android.camera:id/shutter_button_icon"));
-        device.waitForIdle(3000);
-        for (int i = 0; i < 5; i++) {
-            capture1.click();
-            Thread.sleep(3000);
-        }
-       // capture1.click();
-        //Thread.sleep(1000);
-       // device.waitForIdle(6000);
-       // UiObject2 back = device.findObject(By.res("com.android.camera:id/camera_picker"));
-       //back.click();
-       // device.waitForIdle(3000);
-       // capture1.click();
 
+       // device.waitForIdle(3000);
+        //capture1.click();
+
+       // device.waitForIdle(6000);
+        UiObject2 back = device.findObject(By.res("com.android.camera:id/camera_picker"));
+        device.waitForIdle(3000);
+       back.click();
+       device.waitForIdle(4000);
+        UiObject2 capture1 = device.findObject(By.res("com.android.camera:id/shutter_indicator_view"));
+        device.waitForIdle(3000);
+        capture1.click();
+       // device.waitForIdle(4000);
        // UiObject2 calculatorApp = device.findObject(By.text("Calculator"));
         //calculatorApp.swipe();
 
